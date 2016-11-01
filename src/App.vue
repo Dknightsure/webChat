ae3dae3a3620d998031c597ba4b70d457065c11d
Sure <husure.awe@gmail.com>
2016-11-01 15:51:22 +0800

    add login

--- src/App.vue
+++ src/App.vue
@@ -110,4 +110,15 @@ ul.nav{
   display: none;
 }
 
+html,
+body{
+  width: 100%;
+  height: 100%;
+}
+
+#app{
+  width: 100%;
+  height: 100%;
+}
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <h1>WebChat</h1>
+    <label for="username" class="label-user">Username: <input id="username" type="text" class="username" placeholder="Username.."></label>
+    <label for="pwd" class="label-pwd">Password: <input id="pwd" type="password" class="pwd" placeholder="Password.."></label>
+    <button type="button" name="button">Sign in</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+button{
+  width: 200px;
+  height: 100px;
+  font-size: 50px;
+  line-height: 100px;
+  background-color: #75cc5b;
+  color: #ffffff;
+  display: block;
+  margin: 0 auto;
+  border: 1px solid #ffffff;
+  border-radius: 6px;
+  margin-bottom: 50px;
+}
+
+.container{
+  width: 100%;
+  height: 100%;
+  background-color: #75cc5b;
+  color: #ffffff;
+}
+
+h1{
+  font-size: 80px;
+  text-align: center;
+  padding-top: 300px;
+}
+
+label{
+  display: block;
+  font-size: 50px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.label-user{
+  margin: 300px auto 0 auto;
+}
+
+.label-pwd{
+  margin: 50px auto 0 auto;
+}
+
+.username,
+.pwd{
+  width: 500px;
+  height: 60px;
+  font-size: 50px;
+  padding: 10px;
+  border: none;
+  border-radius: 6px;
+}
+
+.username{
+}
+
+.pwd{
+  margin: 50px auto;
+}
+
+</style>
